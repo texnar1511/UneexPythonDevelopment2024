b5f09dd82795978d35064702e481bd4c1a3abcbb
texnar1511 <texnar1511@gmail.com>
2024-02-28 23:21:29 +0300

    Теперь модуль bullscows можно запустить как игру
    Параметры игры: словарь и длина слова. В папке dicts лежат варианты
    словарей

--- bullscows.py
+++ bullscows.py
@@ -1,8 +1,10 @@
-import random
+import random, argparse, urllib.request
 
 
 def bullscows(guess: str, secret: str) -> (int, int):
-    
+    '''
+        Возвращает количество "быков" и "коров" из guess в secret
+    '''    
     bulls, cows = 0, 0
 
     for i, j in zip(guess, secret):
@@ -15,7 +17,9 @@ def bullscows(guess: str, secret: str) -> (int, int):
 
 
 def ask(prompt: str, valid: list[str] = None) -> str:
-    
+    '''
+        Спрашивает слово у пользователя
+    '''
     a = input(prompt)
 
     if valid:
@@ -26,11 +30,16 @@ def ask(prompt: str, valid: list[str] = None) -> str:
 
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
+    '''
+        Выводит результат пользователю
+    '''
     print(format_string.format(bulls, cows))
 
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
-    
+    '''
+        Обеспечивает геймплей игры "Быки и коровы"
+    '''
     secret = random.choice(words)
     tries = 0
 
@@ -38,12 +47,28 @@ def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
         
         guess = ask("Введите слово: ", words)
         tries += 1
-        b, c = bullcows(guess, secret)
+        b, c = bullscows(guess, secret)
         inform("Быки: {}, Коровы: {}", b, c)
         if b == len(guess):
             break
 
-    return f'Вы победили за {tries} попыток'
+    return tries
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('dictionary', type = str, help = 'dictionary for game as file or URL')
+parser.add_argument('length', nargs = '?', default = 5, type = int, help = 'length of secret word')
+args = parser.parse_args()
 
+words = []
+
+if args.dictionary[:4] == 'http':
+    with urllib.request.urlopen(args.dictionary) as f:
+        words = f.read().decode('utf-8').splitlines()
+else:
+    with open(args.dictionary) as f:
+        words = f.read().splitlines()
+
+words = [i for i in words if len(i) == args.length] 
+
+print(gameplay(ask, inform, words))
